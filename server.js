2aaa46b226deabe06f3471ab0f4a299ddd8c648b
anushkanavin-afk <anushka.navin@bscpsyh.christuniversity.in>
2026-03-21 23:05:44 +0530

    fix: stray CSS, stats, homepage routing, footer links, meta tags, terms

--- server.js
+++ server.js
@@ -288,9 +288,9 @@ app.use(helmet({
 }));
 app.use(express.json({ limit: '16kb' }));
 
-// Serve index.html (landing page) at root
+// Serve app.html at root
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'public', 'app.html'));
 });
 
 app.use(express.static(path.join(__dirname, 'public')));
